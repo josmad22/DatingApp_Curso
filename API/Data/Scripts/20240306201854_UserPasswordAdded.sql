f9a4023beba320fcce149366f3fe64490b96ec0d
Jose Madueño <josmad.e95@gmail.com>
2024-03-06 18:21:48 -0500

    Añadir password a la entidad de usuario

---
+++ API/Data/Scripts/20240306201854_UserPasswordAdded.sql
@@ -0,0 +1,11 @@
+﻿BEGIN TRANSACTION;
+
+ALTER TABLE "User" ADD "PasswordHash" BLOB NULL;
+
+ALTER TABLE "User" ADD "PasswordSalt" BLOB NULL;
+
+INSERT INTO "__EFMigrationsHistory" ("MigrationId", "ProductVersion")
+VALUES ('20240306201854_UserPasswordAdded', '7.0.16');
+
+COMMIT;
+
